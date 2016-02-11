7565d84f633d24ef57f5b77f626a7db7ffc5ee65
Erica Geiser <erica@socialtables.com>
2016-02-11 15:30:02 -0500

    add more tests

--- src/__tests__/Tabs-test.js
+++ src/__tests__/Tabs-test.js
@@ -3,6 +3,7 @@
 jest.dontMock("../components/Tabs/Tabs");
 
 import React from "react";
+import ReactDOM from "react-dom";
 import TestUtils from "react-addons-test-utils";
 
 // Babel would move an import in front of the jest.dontMock. That"s why require
@@ -14,12 +15,19 @@ describe("Tabs", () => {
 	describe("without any properties", () => {
 		let tabs;
 		let tabsNode;
+		let firstTabNode;
+		let secondTabNode;
 
 		beforeEach(() => {
 			tabs = TestUtils.renderIntoDocument(
-				<Tabs className="tabs"><Tab></Tab></Tabs>
+				<Tabs className="tabs">
+					<Tab className="firstTab"></Tab>
+					<Tab className="secondTab"></Tab>
+				</Tabs>
 			);
 			tabsNode = TestUtils.findRenderedDOMComponentWithClass(tabs, "tabs");
+			firstTabNode = TestUtils.findRenderedDOMComponentWithClass(tabs, "firstTab");
+			secondTabNode = TestUtils.findRenderedDOMComponentWithClass(tabs, "secondTab");
 		});
 
 		it("tabs should not allow user to override container styles", () => {
@@ -30,19 +38,61 @@ describe("Tabs", () => {
 			expect(tabsNode.children[0].hasAttribute("style")).toBeTruthy();
 			expect(tabsNode.children[1].hasAttribute("style")).toBeTruthy();
 		});
+
+		it("should default the initial selected index to 0", () => {
+			expect(firstTabNode.hasAttribute("selected")).toBe(true);
+			expect(secondTabNode.hasAttribute("selected")).toBe(false);
+		});
+	});
+
+	it("should be able to change the initial selected index", () => {
+		const tabs = TestUtils.renderIntoDocument(
+			<Tabs className="tabs" initialSelectedIndex={1}>
+				<Tab className="firstTab"></Tab>
+				<Tab className="secondTab"></Tab>
+				<Tab className="thirdTabNode"></Tab>
+			</Tabs>
+		);
+		const tabsNode = TestUtils.findRenderedDOMComponentWithClass(tabs, "tabs");
+		const firstTabNode = TestUtils.findRenderedDOMComponentWithClass(tabs, "firstTab");
+		const secondTabNode = TestUtils.findRenderedDOMComponentWithClass(tabs, "secondTab");
+		const thirdTabNode = TestUtils.findRenderedDOMComponentWithClass(tabs, "thirdTabNode");
+
+		expect(firstTabNode.hasAttribute("selected")).toBe(false);
+		expect(secondTabNode.hasAttribute("selected")).toBe(true);
+		expect(thirdTabNode.hasAttribute("selected")).toBe(false);
 	});
 
 	it("should only accept Tab Components as children", () => {
 		const badTabs = TestUtils.renderIntoDocument(
-			<Tabs className="badTabs"><div></div><div></div></Tabs>
+			<Tabs className="badTabs">
+				<div></div>
+				<div></div>
+			</Tabs>
 		);
 		const badTabsNode = TestUtils.findRenderedDOMComponentWithClass(badTabs, "badTabs");
 		expect(badTabsNode.children[0].children[0].children.length).toBe(0);
 
 		const oneDivTabs = TestUtils.renderIntoDocument(
-			<Tabs className="oneDivTabs"><div></div><Tab></Tab></Tabs>
+			<Tabs className="oneDivTabs">
+				<div></div>
+				<Tab></Tab>
+			</Tabs>
 		);
 		const oneDivTabsNode = TestUtils.findRenderedDOMComponentWithClass(oneDivTabs, "oneDivTabs");
 		expect(oneDivTabsNode.children[0].children[0].children.length).toBe(1);
+	});
+
+	it("should be able to specify a label for the set of tabs", () => {
+		const tabs = TestUtils.renderIntoDocument(
+			<Tabs className="tabs" label={"Account Settings"}>
+				<Tab className="firstTab"></Tab>
+				<Tab className="secondTab"></Tab>
+				<Tab className="thirdTabNode"></Tab>
+			</Tabs>
+		);
+		const tabsNode = TestUtils.findRenderedDOMComponentWithClass(tabs, "tabs");
+		expect(tabsNode.hasAttribute("label")).toBe(true);
+		expect(tabsNode.children[0].children[0].innerHTML).toBe("Account Settings");
 	});
 });
