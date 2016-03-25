d93074e88cd5e0928938755d2d179d20eb3ab8a0
Rohit Kalkur <rohit.kalkur@gmail.com>
2016-03-25 14:32:35 -0400

    fixing tests by adding React to global namespace of test suite

--- src/__tests__/utils/setupTestFrameworkScriptFile.js
+++ src/__tests__/utils/setupTestFrameworkScriptFile.js
@@ -1,4 +1,9 @@
 /*
+* Attach React to global namespace
+*/
+window.React = require("react");
+
+/*
 * Create dumb polyfill for requestAnimationFrame
 */
 window.requestAnimationFrame = function(cb) {
