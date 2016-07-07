38f4c063de018d58fbfa7a756feb8d478c098909
Rohit Kalkur <rohit.kalkur@gmail.com>
2016-07-07 01:35:22 -0400

    fix linting issue

--- src/utils/ConfiguredRadium.js
+++ src/utils/ConfiguredRadium.js
@@ -1,5 +1,5 @@
 import Radium from "radium";
 
 export default function ConfiguredRadium(component) {
-  return Radium({ userAgent: "all" })(component);
+	return Radium({ userAgent: "all" })(component);
 }
