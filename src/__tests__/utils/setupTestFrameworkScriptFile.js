a7b65cc0976baeb4039263e273f1b5ebbafe65ff
Rohit Kalkur <rohit.kalkur@gmail.com>
2016-08-25 20:19:44 -0400

    Unit Test Suite: proxy all console.error() calls to throw run-time error on React Warnings

--- src/__tests__/utils/setupTestFrameworkScriptFile.js
+++ src/__tests__/utils/setupTestFrameworkScriptFile.js
@@ -11,3 +11,18 @@ window.React = require("react");
 window.requestAnimationFrame = function(cb) {
 	window.setTimeout(cb, 100);
 };
+
+/*
+* Throw a run-time error on React Warnings that occur in test-suite
+* This will help ensure that we catch these types of issues in our CI check versus at run-time
+*/
+const errorFn = window.console.error;
+window.console.error = function(msg) {
+	window.console.log("ROLO: ", msg);
+
+	// Detect react warnings & error
+	if (/^Warning: /.test(msg)) {
+		throw new Error(`React: ${msg}`);
+	}
+	return errorFn.apply(this, arguments);
+};
