4e75143330722cc8b348f7ec2c8d59e71f90e24a
ddombrow <daniel@socialtables.com>
2020-04-15 00:35:39 -0400

    theme and some cleanup

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import stuiTheme from "./stui-theme";
+
+addons.setConfig({
+  theme: stuiTheme,
+});
---
+++ .storybook/stui-theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: 'light',
+  colorPrimary: 'hotpink',
+  colorSecondary: 'deepskyblue',
+  brandTitle: 'ST-UI Toolkit',
+  brandUrl: 'http://socialtables.github.io/ST-UI-Toolkit',
+});
